8a4b2a385098b76b37c5749d3a769467f0b9a5f0
Christian Vermylen <christian.a.vermylen@gmail.com>
2018-10-14 15:08:29 -0400

    camake build arguments

--- src/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/christian/projects/RESTProxy
-# Build directory: /Users/christian/projects/RESTProxy/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
