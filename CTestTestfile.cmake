59e69691136de0118aa2f6aa55ba36de7e7da460
cvermylen <christian.a.vermylen@gmail.com>
2019-06-08 16:42:23 -0400

    Delete CTestTestfile.cmake

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/christian/CLionProjects/RESTProxy
-# Build directory: /home/christian/CLionProjects/RESTProxy
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
